6c1c23b22e6636c7ed4b0dad9859d6710ea6edc2
ardi667 <149851777+ardi667@users.noreply.github.com>
2025-12-28 04:13:20 +0700

    Fix MongoDB URI for connection

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // === KONEKSI MONGODB ===
-const MONGODB_URI = process.env.MONGODB_URL || 'mongodb+srv://inklusiteknologi:Bandung123@inklusiteknologi.g9awxur.mongodb.net/inklusi-teknologi?retryWrites=true&w=majority';
+const MONGODB_URI = process.env.MONGODB_URL || 'mongodb+srv://inklusiteknologi:Bandung123@inklusiteknologi.g9awxur.mongodb.net/inklusiteknologi?retryWrites=true&w=majority';
 mongoose.connect(MONGODB_URI);
 
 // === MODEL ===
